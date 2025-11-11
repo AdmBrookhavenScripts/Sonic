eb7119700fbc680b65f34ad7013d6a02bf88319f
AdmBrookhavenScripts <168764106+AdmBrookhavenScripts@users.noreply.github.com>
2025-11-11 18:33:26 -0300

    Refactor slot assignments and clean up code
    Updated slot assignments and removed unused code for outfit loading and emitter application.

--- Sonic.lua
+++ Sonic.lua
@@ -6,17 +6,11 @@ target = target:WaitForChild("Head")
 local GunSoundRemote = game:GetService("ReplicatedStorage").RE:FindFirstChild("1Gu1nSound1s")
 local OutfitRemote = game:GetService("ReplicatedStorage").Remotes.LoadOutfit
 
-local SLOT_1 = getgenv().Sonic_Slot or 0
-local SLOT_2 = getgenv().SuperSonic_Slot or 0
+local SLOT_2 = getgenv().Sonic_Slot or 4
+local SLOT_1 = getgenv().SuperSonic_Slot or 5
 local currentSlot = SLOT_1
 
 local TARGET_ID = "8404814227"
-
-local args = {
-    [1] = SLOT_1
-}
-
-game:GetService("ReplicatedStorage").Remotes.LoadOutfit:InvokeServer(unpack(args))
 
 local function extractId(raw)
     return raw and string.match(raw, "%d+")
@@ -25,6 +19,8 @@ end
 local function sendSlot(v)
     OutfitRemote:InvokeServer(v)
 end
+
+sendSlot(SLOT_2)
 
 local function trackSound(sound)
     if sound:IsA("Sound") then
@@ -38,22 +34,8 @@ local function trackSound(sound)
                 sendSlot(currentSlot)
                 if currentSlot == SLOT_1 then
                     currentSlot = SLOT_2
-                    task.wait()
-local args = {
-    [1] = 18635841291,
-    [2] = "021TwinkleYellow"
-}
-
-game:GetService("ReplicatedStorage").Remotes.ApplyEmmiter:InvokeServer(unpack(args))
                 else
                     currentSlot = SLOT_1
-                    task.wait()
-local args = {
-    [1] = 18635841291,
-    [2] = "001Remove"
-}
-
-game:GetService("ReplicatedStorage").Remotes.ApplyEmmiter:InvokeServer(unpack(args))
                 end
             end
         end)
@@ -68,3 +50,5 @@ target.ChildAdded:Connect(function(child)
     task.wait()
     trackSound(child)
 end)
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/Azizanzz0/FE-R15-Sonic-X-Super/refs/heads/main/Protected%20SonicXSuper.txt"))()

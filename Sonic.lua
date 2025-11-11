3aac17c5e9831a0181aba0fec30a0ae216998134
AdmBrookhavenScripts <168764106+AdmBrookhavenScripts@users.noreply.github.com>
2025-11-11 15:14:39 -0300

    Update Sonic.lua

--- Sonic.lua
+++ Sonic.lua
@@ -6,11 +6,17 @@ target = target:WaitForChild("Head")
 local GunSoundRemote = game:GetService("ReplicatedStorage").RE:FindFirstChild("1Gu1nSound1s")
 local OutfitRemote = game:GetService("ReplicatedStorage").Remotes.LoadOutfit
 
-local SLOT_1 = getgenv().Sonic_Slot or 8
-local SLOT_2 = getgenv().SuperSonic_Slot or 10
+local SLOT_1 = getgenv().Sonic_Slot or 0
+local SLOT_2 = getgenv().SuperSonic_Slot or 0
 local currentSlot = SLOT_1
 
 local TARGET_ID = "8404814227"
+
+local args = {
+    [1] = SLOT_1
+}
+
+game:GetService("ReplicatedStorage").Remotes.LoadOutfit:InvokeServer(unpack(args))
 
 local function extractId(raw)
     return raw and string.match(raw, "%d+")
@@ -32,8 +38,22 @@ local function trackSound(sound)
                 sendSlot(currentSlot)
                 if currentSlot == SLOT_1 then
                     currentSlot = SLOT_2
+                    task.wait()
+local args = {
+    [1] = 18635841291,
+    [2] = "021TwinkleYellow"
+}
+
+game:GetService("ReplicatedStorage").Remotes.ApplyEmmiter:InvokeServer(unpack(args))
                 else
                     currentSlot = SLOT_1
+                    task.wait()
+local args = {
+    [1] = 18635841291,
+    [2] = "001Remove"
+}
+
+game:GetService("ReplicatedStorage").Remotes.ApplyEmmiter:InvokeServer(unpack(args))
                 end
             end
         end)
